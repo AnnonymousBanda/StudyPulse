be8fae44ee666161f3c6d144f3b48f09578512d6
Ankit Bhagat <annonymous007banda@gmail.com>
2024-08-04 10:07:56 +0530

    added the themed svg icons

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Logo, Light, Setting, Hamburger } from '@/public'
+import { Logo, Theme, Setting, Hamburger } from '@/public'
 
 const Navbar = () => {
     return (
@@ -8,14 +8,14 @@ const Navbar = () => {
           <nav className='max-container flex items-center justify-between w-full'>
             <div>
               <Link href='/'>
-                <Image src={Logo} alt='logo of StudyPulse' width={200} height={40} className='md:block hidden' />
+                <Image src={Logo.LogoLight} alt='logo of StudyPulse' width={200} height={40} className='md:block hidden' />
               </Link>
               <Image src={Hamburger} alt='menu button' width={25} height={25} className='md:hidden block' />
             </div>
             <div className='flex gap-[15px] items-center'>
-              <Image src={Light} alt='light theme' width={40} height={40} className='md:block hidden' />
+              <Image src={Theme.Light} alt='light theme' width={40} height={40} className='md:block hidden' />
               <div className="w-[6.5rem] h-[6.5rem] rounded-full bg-light-primary"></div>
-              <Image src={Setting} alt='settings' width={25} height={25} className='md:block hidden' />
+              <Image src={Setting.SettingLight} alt='settings' width={25} height={25} className='md:block hidden' />
             </div>
           </nav>
         </div>
--- public/index.js
+++ public/index.js
@@ -1,11 +1,70 @@
-import Logo from '@/public/logo.svg'
-import Light from '@/public/light.svg'
-import Setting from '@/public/setting.svg'
-import Hamburger from '@/public/hamburger.svg'
-import Focus from '@/public/focus.svg'
-import Home from '@/public/home.svg'
-import Lecture from '@/public/lecture.svg'
-import Schedule from '@/public/schedule.svg'
-import Stats from '@/public/stats.svg'
+import LogoLight from '@/public/logo-light.svg'
+import LogoDark from '@/public/logo-dark.svg'
+const Logo = {
+  LogoLight,
+  LogoDark
+}
 
-export { Logo, Light, Setting, Hamburger, Focus, Home, Lecture, Schedule, Stats };
+import Light from '@/public/light.svg'
+import Dark from '@/public/dark.svg'
+const Theme = {
+  Light,
+  Dark
+}
+
+import SettingLight from '@/public/setting-light.svg'
+import SettingDark from '@/public/setting-dark.svg'
+const Setting = {
+  SettingLight,
+  SettingDark
+}
+
+import FocusInactive from '@/public/focus.svg'
+import FocusLight from '@/public/focus-light.svg'
+import FocusDark from '@/public/focus-dark.svg'
+const Focus = {
+  FocusInactive,
+  FocusLight,
+  FocusDark
+}
+
+import HomeInactive from '@/public/home.svg'
+import HomeLight from '@/public/home-light.svg'
+import HomeDark from '@/public/home-dark.svg'
+const Home = {
+  HomeInactive,
+  HomeLight,
+  HomeDark
+}
+
+import LectureInactive from '@/public/lecture.svg'
+import LectureLight from '@/public/lecture-light.svg'
+import LectureDark from '@/public/lecture-dark.svg'
+const Lecture = {
+  LectureInactive,
+  LectureLight,
+  LectureDark
+}
+
+import ScheduleInactive from '@/public/schedule.svg'
+import ScheduleLight from '@/public/schedule-light.svg'
+import ScheduleDark from '@/public/schedule-dark.svg'
+const Schedule = {
+  ScheduleInactive,
+  ScheduleLight,
+  ScheduleDark
+}
+
+import StatsInactive from '@/public/stats.svg'
+import StatsLight from '@/public/stats-light.svg'
+import StatsDark from '@/public/stats-dark.svg'
+const Stats = {
+  StatsInactive,
+  StatsLight,
+  StatsDark
+}
+
+import Hamburger from '@/public/hamburger.svg'
+
+
+export { Logo, Theme, Setting, Hamburger, Focus, Home, Lecture, Schedule, Stats };
