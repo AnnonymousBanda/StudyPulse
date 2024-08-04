c135415f97c1bffa18e508fb225b091e04b8d7e2
Ankit Bhagat <annonymous007banda@gmail.com>
2024-08-04 08:20:12 +0530

    Initial setup of theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "light-primary" : "#006ABC",
+        "light-bg" : "#fff",
+        "dark-primary" : "#343C6A",
+        "dark-bg" : "#000",
+      }
     },
   },
   plugins: [],
