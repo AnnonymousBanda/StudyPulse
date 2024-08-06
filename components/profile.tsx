58cfa348e8cac3cf1bf49ffd2c1e4737186a644d
Ankit Bhagat <annonymous007banda@gmail.com>
2024-08-07 00:13:31 +0530

    decreased the size of dark logo and profile

--- components/profile.tsx
+++ components/profile.tsx
@@ -8,7 +8,7 @@ const profile = () => {
   const { theme } = useTheme()
   const profileClasses = clsx({'border-light-primary': theme==='light', 'border-dark-primary': theme==='dark'})
   return (
-    <div className={`w-[5rem] h-[5rem] rounded-full border-2 overflow-hidden cursor-pointer ${profileClasses}`}>
+    <div className={`w-[4rem] h-[4rem] rounded-full border-2 overflow-hidden cursor-pointer ${profileClasses}`}>
         <Image src={ProfilePic} alt='profile pic' className="w-full h-full object-cover"/>
     </div>
   )
